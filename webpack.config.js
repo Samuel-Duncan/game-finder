1e010f67f117c0e054dd73d9d597100040efa723
Samuel Duncan <samuel.ira.duncan@gmail.com>
2023-12-20 17:16:32 -0500

    Fix issue with webpack-dev-server
    Previously, webpack dev server was not displaying changes in the browser
    so I opted to uninstall it and go with webpack watch.

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,9 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  optimization: {
-    runtimeChunk: 'single',
   },
   module: {
     rules: [
